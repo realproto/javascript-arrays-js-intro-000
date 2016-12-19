0d2d5c9dcc3427424a68dcb92ce069b4f2da0f38
Elvis Delahoz <elvisdelahoz@gmail.com>
2016-12-19 01:17:18 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -9,17 +9,23 @@ function addElementToBeginningOfArray(myarray1, foo) {
 addElementToBeginningOfArray(myarray1, "foo"); 
 var foo; var myarray2 = ["entry0", "entry1"];
 function destructivelyAddElementToBeginningOfArray(myarray2, foo) { 
-    var myarray2 = [foo, ...myarray2];
+    var myarray2[0] = [foo];
     var myarrayNew = myarray2;
     myarrayNew;
 }
 destructivelyAddElementToBeginningOfArray(myarray2, 'foo');
-function addElementToEndOfArray() {
+var foo; var myarray3 = ["entry0", "entry1"];
+function destructivelyAddElementToEndOfArray(myarray3, foo) {
+  var myarray3 = [myarray3..., `foo`];
+  var mynewarray = myarray3;
+  return mynewarray;
 
-  
 }
-addElementToEndOfArray();
-function destructivelyAddElementToEndOfArray() {
-
+destructivelyAddElementToEndOfArray(myarray3, 'foo');
+var foo; var myarray4 = ["entry0", "entry1"];
+function addElementToEndOfArray(myarray4, foo) {
+  var myarray4 = [myarray4..., `foo`];
+  var mynewarray = myarray4;
+  return mynewarray;
 }
-destructivelyAddElementToEndOfArray;
+addElementToEndOfArray(myarray4, 'foo');
