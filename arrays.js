2eab5cb85edb5eb5ef18bf8b5390b14071751acf
Elvis Delahoz <elvisdelahoz@gmail.com>
2016-12-19 02:09:51 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,31 +1,24 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 chocolateBars;
-var foo; var myarray1 = ["entry0", "entry1"];
-function addElementToBeginningOfArray(myarray1, foo) { 
-    var myarray1 = [`foo`, ...myarray1];
-    var mynewarray = myarray1;
-    return mynewarray;
+var foo; var myarray = ["entry0", "entry1"];
+function addElementToBeginningOfArray(myarray, foo) { 
+    var myarray = [`foo`, ...myarray];
+    myarray;
+    }
+addElementToBeginningOfArray(myarray, 1); 
+function destructivelyAddElementToBeginningOfArray(myarray, foo) {  
+    var myarray = [`foo`, ...myarray];
+    myarray;
+    }
+destructivelyAddElementToBeginningOfArray(myarray, 1);
+function destructivelyAddElementToEndOfArray(myarray, foo) {
+  myarray = [...myarray, `foo`];
+  myarray;
+  }
+destructivelyAddElementToEndOfArray(myarray, 1);
+function addElementToEndOfArray(myarray, foo) {
+  var myarray = [...myarray, `foo`];
+  var mynewarray = myarray;
+  mynewarray;
 }
-addElementToBeginningOfArray(myarray1, "foo"); 
-var foo; var myarray2 = ["entry0", "entry1"];
-function destructivelyAddElementToBeginningOfArray(myarray2, foo) { 
-    var myarray2[0] = [foo];
-    var myarrayNew = myarray2;
-    myarrayNew;
-}
-destructivelyAddElementToBeginningOfArray(myarray2, 'foo');
-var foo; var myarray3 = ["entry0", "entry1"];
-function destructivelyAddElementToEndOfArray(myarray3, foo) {
-  var myarray3 = [myarray3..., `foo`];
-  var mynewarray = myarray3;
-  return mynewarray;
-
-}
-destructivelyAddElementToEndOfArray(myarray3, 'foo');
-var foo; var myarray4 = ["entry0", "entry1"];
-function addElementToEndOfArray(myarray4, foo) {
-  var myarray4 = [myarray4..., `foo`];
-  var mynewarray = myarray4;
-  return mynewarray;
-}
-addElementToEndOfArray(myarray4, 'foo');
+addElementToEndOfArray(myarray, 1);
